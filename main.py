a18a2b759fc7df96d656afc46e99aa87c4904ae1
igorM <mourtaza@garamchai.localdomain>
2021-05-02 17:56:21 +0200

    main.py envoie helloworld

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-## code en python, inshallah
+# code en python, inshallah
+
+import tweepy 
+import json
+import random 
+import credentials # fichier pour se connecter à l'app
+
+api,auth = credentials.auth()
+api.update_status(status="hello world")
+
+
+
 
